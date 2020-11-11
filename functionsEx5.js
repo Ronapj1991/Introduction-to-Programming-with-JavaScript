6ee82c5fac34d7486a4bf37cb4491d3187ca4b02
Ron Javellana <RAdoublegaming@gmail.com>
2020-11-11 04:26:40 +0000

    practiced on Nov 10, 2020

---
+++ functionsEx5.js
@@ -0,0 +1,11 @@
+function scream(words) {
+  words = words + '!!!!';
+  return;
+  console.log(words);
+}
+
+scream('Yipeee');
+
+//this does not log anything to the console
+
+//return causes Javascript to terminate on line 3
---
+++ functionsEx6.js
@@ -0,0 +1,8 @@
+function scream(words) {
+  return words + '!!!!';
+}
+
+scream('Yipeee');
+
+// this does not log anything to the console
+// however the value of scream('Yipee') is Yipeee!!!!
